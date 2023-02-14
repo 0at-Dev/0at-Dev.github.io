299cc2737cfd64085624cf7e3c3807bb71ceab09
0at-Dev <oatcdeveloper@gmail.com>
2023-02-14 10:17:04 -0500

    first commit (protfolio mockup)

---
+++ src/index.js
@@ -0,0 +1,61 @@
+window.addEventListener("DOMContentLoaded",()=>{
+const WORK_SECTION=document.getElementById("Work")
+const ABOUT_SECTION=document.getElementById("About")
+const PREVSLIDE_BUTTON=document.getElementById("PrevSlide")
+const NEXTSLIDE_BUTTON=document.getElementById("NextSlide")
+
+let $workSectionWrapper=WORK_SECTION.querySelector("div>div")
+let $workSlidesWrapper=$workSectionWrapper.querySelectorAll("div")[0]
+let $workSlides=$workSlidesWrapper.querySelectorAll("div")
+let _workSlideActive=0
+
+const revealSectionContent=()=>{
+    let aboutSectionBounderies=ABOUT_SECTION.getBoundingClientRect()
+    let {top:aboutTop, y:aboutY}=aboutSectionBounderies
+        
+        if(Math.floor(aboutTop)<=0 || Math.floor(aboutY)<=0){
+            let $title=ABOUT_SECTION.querySelector("div>h2")
+            let $description=ABOUT_SECTION.querySelector("div>p")
+
+                if($title.classList.contains("hide-to-left")){
+                    $title.classList.remove("hide-to-left")
+                    $title.classList.add("reveal-from-left")
+                }
+                if($description.classList.contains("hide-to-left")){
+                    $description.classList.remove("hide-to-left")
+                    $description.classList.add("reveal-from-left")
+                }
+        }
+}
+// Listen
+    revealSectionContent()
+
+    document.addEventListener("scroll",revealSectionContent)
+    NEXTSLIDE_BUTTON.addEventListener("click",()=>{
+        let $currentSlide=$workSlides[_workSlideActive]
+        let _newSlideIndex=_workSlideActive + 1
+        let _indexLimit= $workSlides.length - 1
+        let $newActiveSlide=$workSlides[_newSlideIndex]
+            
+            if(_workSlideActive<_indexLimit){
+                $currentSlide.style.opacity="0"
+                $newActiveSlide.style.opacity="1"
+                _workSlideActive=_workSlideActive + 1  
+            }
+        
+    })
+
+    PREVSLIDE_BUTTON.addEventListener("click",()=>{
+        let $currentSlide=$workSlides[_workSlideActive]
+        let _newSlideIndex=_workSlideActive - 1
+        let _indexLimit= $workSlides.length - 1
+        let $newActiveSlide=$workSlides[_newSlideIndex]
+            
+            if(_workSlideActive>0){
+                $currentSlide.style.opacity="0"
+                $newActiveSlide.style.opacity="1"
+                _workSlideActive=_workSlideActive - 1  
+            }
+    })
+
+})
