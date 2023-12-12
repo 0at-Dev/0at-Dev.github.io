b7fb1bc1701580f0c3001087b937994e9921ea95
0at-Dev <oatcdeveloper@gmail.com>
2023-12-12 10:09:28 -0500

    redesigning

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 
 import { highlighted } from './constants/projects'
 import animate from './helpers/animations'
-import insert from './helpers/insertion'
-import swiperSlider from './helpers/swiper'
-import translate from './helpers/translations'
 import './style.css'
-
-import Swiper from "swiper/bundle";
-import 'swiper/css/bundle'
-
+import SplitType from 'split-type'
 import { gsap } from "gsap";
 import { 
     ScrollTrigger,
@@ -17,68 +11,13 @@ import {
 // Plugins 
 gsap.registerPlugin(ScrollTrigger)
 
-const initSwiper=()=>{
-
-    return new Swiper("#Projects", {
-        effect: "creative",
-        grabCursor: true,
-        loop:true,
-        speed:500,
-        creativeEffect: {
-          prev: {
-            opacity:.9,
-            translate: ["-20%", 0, -1],
-          },
-          next: {
-            translate: ["100%", 0, 0],
-          },
-        },
-    });
-}
-
-const insertProjectsSlider=()=>{
-    const $projectTemplate=document.querySelector("template[template-name='project-slider-slide']")
-
-    if(!$projectTemplate.content)return
-
-    let $projectsContainer=document.getElementById("Projects")
-    let $fragment=document.createDocumentFragment()
-    
-    let $sliderWrapper=document.createElement("ul")
-        $sliderWrapper.classList.add("swiper-wrapper")
-
-        highlighted().reverse().forEach(project=>{
-            let {id, name, description,media,site}=project
-
-            let $clone=document.importNode($projectTemplate.content, true)
-
-            let $slide=$clone.querySelector("li")
-                $slide.id=id
-            let $name=$clone.querySelector("span[project-name='']")
-            let $description=$clone.querySelector("p[project-description='']")
-            let $sitelink=$clone.querySelector("a[project-site-link='']")
-            let $media=$clone.querySelector("img[project-media='']")
-
-            $name.textContent=name
-            $description.textContent=description
-            $sitelink.href=site
-            $media.src=media[0]
-
-            $fragment.appendChild($slide)
-        })
-
-        $sliderWrapper.appendChild($fragment)
-        $projectsContainer.appendChild($sliderWrapper)
-
-        initSwiper()
-}
-
 const initShapedTextEffect=()=>{
-  const $scrollAnchorEl=document.querySelector('a[href="#Projects"] div')
+  const $scrollAnchorEl=document.querySelector('a[href="#Work"] div')
   const scrollAnchorTextContent=$scrollAnchorEl.textContent
   const scrollAnchorChars=scrollAnchorTextContent.split('')
   $scrollAnchorEl.textContent=''
   scrollAnchorChars.forEach((char,index)=>{
+
     let $span=document.createElement("span")
         $span.style.transform=`rotate(${index * 13.5}deg)`
         $span.textContent=char
@@ -87,45 +26,77 @@ const initShapedTextEffect=()=>{
   })
 }
 
-const changeNavStylesOnViewport=()=>{
-  // gsap.set(".main-nav-toggler-burguer",{backgroundColor:"#5e5e63"})
-  // gsap.to(".main-nav-toggler-burguer",{
-  //   scrollTrigger:{
-  //     trigger: "#Work",
-  //     // toggleActions: "restart pause reverse pause"
-  //   },
-  //   duration: .1,
-  //   ease: "elastic",
-  //   backgroundColor:"#f6f6f6"
-  // })
-  
- const tween=gsap.to(".main-nav-toggler-burguer",
- {
-    duration: .1,
-    ease: "elastic",
-    backgroundColor:"#f6f6f6"
- })
+const initPinnedProjectsEffect=()=>{
+  let media=gsap.matchMedia()
 
- ScrollTrigger.create({
-    trigger:"#Work",
-    start: 'top 80%',
-    onEnter: () => {
-        console.log("ps entro")
-      tween.play()},
-    onLeave: () => {
-      console.log("ps salio")
-      tween.reverse()
-      console.log(tween)
-    },
- })
+
+  media.add("(min-width: 601px)", ()=>{
+
+    const projects = gsap.utils.toArray("#PinnedProjects>ul>li:not(:first-child)")
+    const projectsPhotos = gsap.utils.toArray("#ListedProjects>ul>li:not(:first-child)")
+
+    gsap.set(projectsPhotos, {yPercent:101})
+
+    const allProjectsPhotos = gsap.utils.toArray("#ListedProjects>ul>li")
+
+    ScrollTrigger.create({
+      trigger:"#Projects",
+      start:"top top",
+      end:"bottom bottom",
+      pin:"#ListedProjects"
+    })
+
+    projects.forEach((detail, index)=> {
+
+      let headline = detail.querySelector("p")
+      let animation = gsap.timeline()
+         .to(projectsPhotos[index], {yPercent:0})
+         .set(allProjectsPhotos[index], {autoAlpha:0})
+      ScrollTrigger.create({
+        trigger:headline,
+        start:"top 80%",
+        end:"top 50%",
+        animation:animation,
+        scrub:true,
+        markers:false
+      })
+    })
+
+    return () => { // optional
+      // custom cleanup code here (runs when it STOPS matching)
+      console.log("mobile")
+    };
+  })
 }
 
+const initSplittedTitle=()=>{
+  const workSectionTitle=new SplitType('#Work h2', { types: 'chars' })
+  const workSectionTitleChars=workSectionTitle.chars
+
+  // ScrollTrigger.create({
+  //   trigger:"#Projects",
+  //   start:"top top",
+  //   end:"bottom bottom",
+  //   pin:"#ListedProjects"
+  // })
+
+  gsap.from(workSectionTitleChars, 1,{
+    stagger:.05,
+    scrollTrigger:"#Work",
+    start: "top center",
+    duration: { min: 0.02, max: 0.2 },
+    scrub: 1,
+    y: 100,
+    ease: "power3.InOut",
+})
+}
+
+
+
 document.addEventListener("DOMContentLoaded",e=>{
-    // translate()
-    // swiperSlider()
-    // insert()
-    insertProjectsSlider()
-    initShapedTextEffect()
-    changeNavStylesOnViewport()
-    // animate()
+  
+    // initShapedTextEffect()
+    initPinnedProjectsEffect()
+    initSplittedTitle()
+ 
 })
